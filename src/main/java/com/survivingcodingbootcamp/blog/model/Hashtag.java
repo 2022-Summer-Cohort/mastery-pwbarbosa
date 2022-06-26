e979cc01e79db10a088160d9bc1b9277834761f3
Pedro <pwbarbosa@gmail.com>
2022-06-26 17:39:54 -0400

    Hashtag pojo creation and Post pojo update commit

---
+++ src/main/java/com/survivingcodingbootcamp/blog/model/Hashtag.java
@@ -0,0 +1,39 @@
+package com.survivingcodingbootcamp.blog.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Arrays;
+import java.util.Collection;
+
+@Entity
+public class Hashtag {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    @ManyToMany
+    private Collection<Post> posts;
+
+    public Hashtag(long id, String name, Post... posts) {
+        this.id = id;
+        this.name = name;
+        this.posts = Arrays.asList(posts);
+    }
+
+    public Hashtag() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Collection<Post> getPosts() {
+        return posts;
+    }
+}
