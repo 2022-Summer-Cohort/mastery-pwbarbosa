7ddf19256d9a568d11312e4479b8a811d6733cd8
Pedro <pwbarbosa@gmail.com>
2022-07-08 13:42:01 -0400

    some of css stretch

--- src/main/java/com/survivingcodingbootcamp/blog/controller/HomeController.java
+++ src/main/java/com/survivingcodingbootcamp/blog/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.survivingcodingbootcamp.blog.controller;
 
+import com.survivingcodingbootcamp.blog.model.Topic;
 import com.survivingcodingbootcamp.blog.repository.TopicRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -18,5 +24,16 @@ public class HomeController {
     public String displayHomePage(Model model) {
         model.addAttribute("topics", topicRepo.findAll());
         return "home-template";
+    }
+
+    @PostMapping("/addTopic")
+    public String addTopic(@RequestParam String newTopic){
+        Optional<Topic> topicOptional = topicRepo.findByNameIgnoreCase(newTopic);
+        if (topicOptional.isEmpty()) {
+            Topic topic1 = new Topic(newTopic);
+            topicRepo.save(topic1);
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/survivingcodingbootcamp/blog/repository/TopicRepository.java
+++ src/main/java/com/survivingcodingbootcamp/blog/repository/TopicRepository.java
@@ -3,5 +3,8 @@ package com.survivingcodingbootcamp.blog.repository;
 import com.survivingcodingbootcamp.blog.model.Topic;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TopicRepository extends CrudRepository<Topic, Long> {
+    Optional<Topic> findByNameIgnoreCase(String name);
 }
